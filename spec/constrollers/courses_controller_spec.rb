4c3d947e4be284b9b45702f882bcfb3b4a073a3c
SurajSJagtap1 <surajsjagtap2022@gmail.com>
2024-07-23 18:04:50 +0530

    courses index api spec

--- spec/constrollers/courses_controller_spec.rb
+++ spec/constrollers/courses_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe CoursesController, type: :controller do
-    
+
+    context 'Creating courses and associated tutors' do
+
+    end
+
+    context 'Listing of Courses and associated tutors' do
+        describe 'GET index' do
+            it 'Returns courses and its associated tutors' do
+                course = create(:course)
+                get :index
+                expect(response.status).to eql(200)
+            end
+        end
+    end
 end
--- spec/factories/course.rb
+++ spec/factories/course.rb
@@ -1,4 +1,10 @@
-factory :course do
-    name { Faker.name }
-    
+require 'faker'
+
+FactoryGirl.define do
+  factory :course do
+    name { Faker::Educator.course }
+    description { Faker::Lorem.sentence }
+    duration { Faker::Number.non_zero_digit }
+    fees { Faker::Commerce.price }
   end
+end
