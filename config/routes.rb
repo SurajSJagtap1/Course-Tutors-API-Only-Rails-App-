c0ca72cc0d22161237ceabec59850bbb90747977
SurajSJagtap1 <surajsjagtap2022@gmail.com>
2024-09-24 18:12:06 +0530

    added route for sidekiq stats UI page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -5,4 +7,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :courses, only: [:create, :index]
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
 end
